740e13b3d4813729ccfa604f7232160a5f7c808a
Giu-lis <giulia.domingues1a@gmail.com>
2026-03-17 15:14:35 -0300

    FINALMENTE - Site - 17/03

---
+++ cadastroLogin/src/main/java/com/giuliadev2a/cadastroLogin/controller/UsuarioController.java
@@ -0,0 +1,112 @@
+package com.giuliadev2a.cadastroLogin.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.giuliadev2a.cadastroLogin.entities.Usuario;
+import com.giuliadev2a.cadastroLogin.services.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService service;
+
+    @PostMapping
+    public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+        Usuario novoUsuario = service.cadastrar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
+
+    boolean removido = service.deletar(id);
+
+    if (!removido) {
+    return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.noContent().build(); // 204
+    //return ResponseEntity.ok(usuarioDeletado);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(
+    @PathVariable Long id,
+    @RequestBody Usuario usuario) {
+
+    Usuario usuarioAtualizado = service.atualizar(id, usuario);
+
+    if (usuarioAtualizado == null) {
+    return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(usuarioAtualizado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        Usuario usuario = service.buscarPorId(id);
+        if (usuario == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(usuario);
+    }
+
+    //Listar todos usuários (GET /usuarios)
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+    return service.listarTodos();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String,
+    String> dados) {
+
+            String email = dados.get("email");
+            String senha = dados.get("senha");
+            Usuario usuario = service.login(email, senha);
+
+            if (usuario == null) {
+                return ResponseEntity.status(401).body("Email ou senha inválidos");
+            }
+
+            return ResponseEntity.ok(usuario);
+    }
+    
+    @PostMapping("/solicitar-recuperacao")
+    public ResponseEntity<Void>solicitarRecuperacao(@RequestParam String email)
+    {
+        boolean enviado = service.enviarEmailRecuperacao(email);
+        
+        if(enviado) {
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.notFound().build();
+            }
+        }
+
+
+
+    @PostMapping("/redefinir-senha")
+    public ResponseEntity<String> redefinirSenha(@RequestBody Map<String, String> payload) {
+        String email = payload.get("email");
+        String novaSenha = payload.get("novaSenha");
+        
+        boolean sucesso = service.redefinirSenha(email, novaSenha);
+        
+        if(sucesso) {
+            return ResponseEntity.ok("Senha redefinida com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+     
+    }
+
+}
---
+++ cadastroLogin/src/main/java/com/giuliadev2a/cadastroLogin/services/UsuarioService.java
@@ -0,0 +1,119 @@
+package com.giuliadev2a.cadastroLogin.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.giuliadev2a.cadastroLogin.entities.Usuario;
+import com.giuliadev2a.cadastroLogin.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<Usuario> listarTodos(){
+        return repository.findAll();
+    }
+   
+    public Usuario cadastrar(Usuario usuario) {
+     // senha criptografada
+        usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+        return repository.save(usuario);
+    }
+    
+    
+    public Usuario atualizar(Long id, Usuario dados) {
+        Usuario usuario = repository.findById(id).orElse(null);
+        if (usuario == null) {
+            return null;
+        }
+
+
+        usuario.setNome(dados.getNome());
+        usuario.setEmail(dados.getEmail());
+        usuario.setSenha(dados.getSenha());
+        usuario.setPerfil(dados.getPerfil());
+        usuario.setEndereco(dados.getEndereco());
+        usuario.setBairro(dados.getBairro());
+        usuario.setComplemento(dados.getComplemento());
+        usuario.setCep(dados.getCep());
+        usuario.setCidade(dados.getCidade());
+        usuario.setEstado(dados.getEstado());
+        usuario.setFoto(dados.getFoto());
+
+        return repository.save(usuario);
+        }
+
+        public boolean deletar(Long id) {
+        if (!repository.existsById(id)) {
+        return false;
+        }
+
+        // localhost:8080/usuarios/3 DELETE
+        repository.deleteById(id);
+        return true;
+        }
+        
+          public Usuario buscarPorId(Long id) {
+                return repository.findById(id).orElse(null);
+            }
+        
+        public Usuario login(String email, String senha) {
+                       
+            Usuario usuario =  repository.findByEmail(email);
+            if (usuario == null) {
+                return null;
+            }
+            // Validar senha com bcrypt
+            if (!passwordEncoder.matches(senha, usuario.getSenha())) {
+                return null;
+            }
+            return usuario;
+        }
+        
+        @Autowired
+        private JavaMailSender mailSender; 
+        
+        public boolean enviarEmailRecuperacao(String email) {
+            Usuario usuario = repository.findByEmail(email);
+            
+            if (usuario != null) {
+                String link = "http://127.0.0.1:5500/redefinir-senha.html?email=" + email;        
+                SimpleMailMessage mensagem = new SimpleMailMessage();
+                mensagem.setTo(email);
+                mensagem.setSubject("Recuperação de Senha - DevSenai");
+                mensagem.setText(
+                        "Olá " + usuario.getNome() + ",\n\n" + "Clique no link abaixo para criar uma nova senha:\n" +
+                link
+              );
+                    
+        mailSender.send(mensagem);
+                
+                System.out.println("Email enviado para: " +email );
+                
+                return true;
+            }
+            
+            return false;
+            }
+        
+        public boolean redefinirSenha(String email, String novaSenha) {
+            Usuario usuario = repository.findByEmail(email);
+            
+            if( usuario != null) {
+                usuario.setSenha(new BCryptPasswordEncoder().encode(novaSenha));
+                repository.save(usuario);
+                return true;
+            }
+            return false;
+        }        
+}
