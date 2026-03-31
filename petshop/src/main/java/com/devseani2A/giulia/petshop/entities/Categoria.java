e09f5029435d7ca5eb1a9aaedd398cf75acbd35e
Giu-lis <giulia.domingues1a@gmail.com>
2026-03-31 15:42:38 -0300

    projeto PetShop BackEbd 31/03

---
+++ petshop/src/main/java/com/devseani2A/giulia/petshop/entities/Categoria.java
@@ -0,0 +1,59 @@
+package com.devseani2A.giulia.petshop.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+
+public class Categoria implements Serializable {
+	
+	@Id()
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	private String descricao;
+	private boolean ativo;
+	
+	public Categoria () {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
